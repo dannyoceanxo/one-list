8a8374c67948c87f10a70a58c42eb9efb74e2760
Daniel Shenker <danshenk619@gmail.com>
2017-05-10 22:15:10 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # One List
+
+One List was one of our more simple projects but gave a much better understanding of usability on a website.  It taught us how to make something that doesn't just meet the guidelines of an assignment but could actually be used in a real app, such as a to-do list that allowed you to check things off as they got done.
--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
-import './styles/screen.scss'
+import './styles/screen.scss' // imports the scss
 
 const main = () => {
   const input = document.querySelector('input')
   const list = document.querySelector('ul.one-list')
   const button = document.querySelector('button')
-  const form = document.querySelector('form')
+  const form = document.querySelector('form') // would love clarification on exactly what all the const options do
 
   button.addEventListener('click', (event) => {
-    event.preventDefault()
+    event.preventDefault() // can't figure out what this does, have an idea but would like to know
     const listItem = input.value
     const li = document.createElement('li')
     li.textContent = listItem
     list.appendChild(li)
-
+    // This is in place so that when someone clicks on the button I created it adds the words they typed in the box into the list we created
+    // the append child is to indicate that it adds on instead of replacing
     li.addEventListener('click', () => {
-      li.style.textDecoration = 'line-through'
+      li.style.textDecoration = 'line-through' // this indicates that we'll add the through line after they've clicked on the added item
     })
-    form.reset()
+    li.addEventListener('dblclick', (event) => {
+      list.removeChild(li) // causes things to be removed when double clicked
+    })
+    form.reset() // clears out the previous input
   })
 }
 
 document.addEventListener('DOMContentLoaded', main)
 
+// Ignore from here down...
+
 if (module.hot) {
-  module.hot.dispose(() => window.location.reload())
+  module.hot.dispose(() => window.location.reload()) // honestly not sure
   module.hot.accept(err => console.error(err))
 }
